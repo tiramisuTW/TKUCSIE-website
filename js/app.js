62b19a4f267639538fed803e20d40a5c7cfdc6aa
Len_AshBell <jason841022@yahoo.com.tw>
2017-02-09 00:47:23 +0800

    first blood - demo

---
+++ js/app.js
@@ -0,0 +1,28 @@
+/**
+ * Created by max on 2017/2/7.
+ */
+
+var timeOut;
+
+$(document).ready(function() {
+
+    $('.list-item').each(function(index) {
+        var node = $(this);        
+        node.mouseover(function () {
+            timeOut = setTimeout(function() {
+                node.animate({
+                    'margin-left': -300,
+                    'opacity': 1
+                }, 300);
+            }, 200);
+
+        }).mouseout(function () {
+            node.animate({
+                'margin-left': 0,
+                'opacity': .5
+            }, 300);
+            clearTimeout(timeOut);
+        });
+    })
+
+});
