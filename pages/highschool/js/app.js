4e93724a6b8877e98d790f9fe5095ef423b05b81
Len_AshBell <jason841022@yahoo.com.tw>
2017-02-18 17:04:39 +0800

    opt high school

--- pages/highschool/js/app.js
+++ pages/highschool/js/app.js
@@ -2,26 +2,52 @@
  * Created by max on 2017/2/17.
  */
 
+
+// wow init
+new WOW().init();
+
+
+
 $(document).ready(function() {
 
+    // gallery owl init
     $("#owl-gallery").owlCarousel({
-        autoPlay: 5000, //Set AutoPlay to 3 seconds
+        autoPlay: 1000,
         navigation: true,
-        items : 3,
-        itemsDesktop : [1199,3],
-        itemsDesktopSmall : [979,3]
+        navigationText: false,
+        pagination: false,
+
+        items: 3,
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [979, 3]
     });
 
+
+    // class hover fn
     $('.outter').each(function(i) {
         var imgbox = $(this).find('.hover');
-        $(this).mouseover(function() {
-            imgbox.css('display', 'block');
-            imgbox.removeClass('animated fadeOut');
-            imgbox.addClass('animated fadeIn');
-        }).mouseleave(function() {
-            imgbox.removeClass('animated fadeIn');
-            imgbox.addClass('animated fadeOut');
-        });
+        $(this)
+            .mouseover(function() {
+                imgbox.css('display', 'block');
+                imgbox.removeClass('animated fadeOut-fast');
+                imgbox.addClass('animated fadeIn-fast');
+            })
+            .mouseleave(function() {
+                imgbox.removeClass('animated fadeIn-fast');
+                imgbox.addClass('animated fadeOut-fast');
+            });
     });
+
 });
 
+// section scroll nav
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
